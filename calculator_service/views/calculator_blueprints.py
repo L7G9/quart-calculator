d271e29ec1f5cc78d3275f21e0529d7477f8adbf
l7g9 <lukewgregory@gmail.com>
2022-09-30 04:44:50 +0000

    Remove commented code

--- calculator_service/views/calculator_blueprints.py
+++ calculator_service/views/calculator_blueprints.py
@@ -2,19 +2,6 @@ from quart import Blueprint
 from quart import request
 from calculator_service.calculator import Calculator
 
-#class Calculator:
- #   def add(self, a, b):
-  #      return a + b
-#
- #   def subtract(self, a, b):
-  ##      return a - b
-#
- #   def multiply(self, a, b):
-  #      return a * b
-
-   # def divide(self, a, b):
-    #    return a / b
-
 calculator = Calculator()
 
 add = Blueprint("add", __name__)
