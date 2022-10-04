452b0a1885f921329a422ffe076d426b886bd4ab
l7g9 <lukewgregory@gmail.com>
2022-10-04 19:47:28 +0000

    Add sum and mean of list

--- calculator_service/calculator.py
+++ calculator_service/calculator.py
@@ -10,3 +10,16 @@ class Calculator:
 
     def divide(self, a, b):
         return a / b
+
+    def sum(self, list):
+        return sum(list)
+
+    def mean(self, list):
+        return sum(list) / len(list)
+
+    def modal(self, list):
+        return 0
+
+    def mode(self, list):
+        return 0
+
--- calculator_service/tests/test_calculator.py
+++ calculator_service/tests/test_calculator.py
@@ -4,6 +4,7 @@ from calculator_service.calculator import Calculator
 class Test(unittest.TestCase):
     def setUp(self):
        self.calculator = Calculator()
+       self.list = [4,5,1,2,3]
 
     def test_add(self):
         self.assertEqual(self.calculator.add(20, 10), 30)
@@ -16,3 +17,9 @@ class Test(unittest.TestCase):
 
     def test_divide(self):
         self.assertEqual(self.calculator.divide(20, 10), 2)
+
+    def test_sum(self):
+        self.assertEqual(self.calculator.sum(self.list), 15)
+
+    def test_mean(self):
+        self.assertEqual(self.calculator.mean(self.list), 3)
